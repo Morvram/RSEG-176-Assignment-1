c1760cb8e98db5b5fee5d486e7cbb2500e89a96b
J.R. Mandell <JRMhere@protonmail.ch>
2022-11-01 08:27:21 -0400

    remove aspose.words watermark from .docx

--- documentConversion.py
+++ documentConversion.py
@@ -15,8 +15,11 @@ def test_pdfToRTF ():
     os.chdir("C:/Users/joshu/Dropbox/Data Science Graduate Program/Brandeis - Master's Strategic Analytics/RSEG 176/Assignment 1/RSEG-176-Assignment-1") #obviously change to whatever your working directory is.
     convertDoc("example_pdf_syllabus.pdf", ".rtf")
     
-def test_rtfToDocx():
+def test_rtfToDocx(): #updated 11/1/2022 to delete watermark.
     convertDoc("example_pdf_syllabus.rtf", ".docx")
+    d = docx.Document("example_pdf_syllabus.docx")
+    delete_paragraph(d.paragraphs[0])
+    d.save("example_pdf_syllabus.docx")
     
 #JM 10/25/2022: tested both of the 'test' methods to make sure they work at creating new files. They do work - copying all the pdf text. However, there is a watermark (in the form of red text on the first page) added by aspose.words
     #TODO use file manipulation to remove the watermark after creating the file?
@@ -48,3 +51,13 @@ def extractPDFText(pdfloc): #pdfloc = filepath of a PDF
     return(text)
     
 t = extractPDFText("example_pdf_syllabus.pdf")
+
+
+
+#11/1/2022: trying to remove text from Docx using Python
+import docx
+def delete_paragraph(paragraph): #deletes the aspose.words watermark from a docx file.
+    p = paragraph._element
+    p.getparent().remove(p)
+    p._p = p._element = None
+    
