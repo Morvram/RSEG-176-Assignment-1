4236f6f8db33f33fdf62a50d39382d7c1009c6df
J.R. Mandell <JRMhere@protonmail.ch>
2022-11-04 15:30:32 -0400

    utter, crushing failure

--- web app with s3 and python flask/config.py
+++ web app with s3 and python flask/config.py
@@ -1,4 +1,5 @@
 import boto
+import os
 
 S3 = 's3'
  
@@ -7,4 +8,10 @@ S3 = 's3'
 # as a standard practice we are not hard-coding the aws access key id and secret key credentials here
 # the credentials are defined in the .credentials file
 def create_s3_client():
-    return boto3.client(S3)
+    os.chdir("C:\\Users\\joshu\\Dropbox\\Data Science Graduate Program\\Brandeis - Master's Strategic Analytics\\RSEG 176\\Assignment 1\\RSEG-176-Assignment-1\\web app with s3 and python flask") #or whatever working directory.
+    with open('.credentials.txt') as f:
+        lines = f.readlines()
+    return boto3.client(S3,
+        aws_access_key_id=lines[0],
+        aws_secret_access_key=lines[1]
+    )
